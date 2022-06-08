98943ae2043e6d2787d5c97088fd761a22f1a87b
xuekaikai <2780739366@qq.com>
2022-06-08 20:42:04 +0800

    修改项目结构

--- app/src/main/java/com/vifia/PageFragmentHome.java
+++ app/src/main/java/com/vifia/PageFragmentHome.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 public class PageFragmentHome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.activity_tab, container, false);
         return view;
     }
 }
